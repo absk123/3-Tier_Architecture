5dc30cf21e8f60ed2a3090feaeed8d1159c3b933
Abhisek Singh <77376796+absk123@users.noreply.github.com>
2023-12-26 09:15:23 +0530

    Update main.tf

--- 6-AzureSQLServer/main.tf
+++ 6-AzureSQLServer/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_sql_server" "todoappsqlserver" {
   for_each                     = var.todo
   name                         = each.key
   resource_group_name          = each.value.rg
-  location                     = each.value.jgh
+  location                     = each.value.location
   version                      = "12.0"
   administrator_login          = each.value.login
   administrator_login_password = each.value.pass
