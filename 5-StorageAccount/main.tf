43e05deced4d2d5016025499ff6573224477bc6a
Abhisek Singh <77376796+absk123@users.noreply.github.com>
2023-12-26 09:01:10 +0530

    Update main.tf

--- 5-StorageAccount/main.tf
+++ 5-StorageAccount/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_storage_account" "todoappstorageaccount1" {
   for_each                 = var.todo
   name                     = each.key
   resource_group_name      = each.value.rg
-  location                 = each.value.jgh
+  location                 = each.value.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
