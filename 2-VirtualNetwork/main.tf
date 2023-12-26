9ef1f5d5381eeffb8cfb732a18e2e8ad52eaa7a3
Abhisek Singh <77376796+absk123@users.noreply.github.com>
2023-12-26 08:59:31 +0530

    Update main.tf

--- 2-VirtualNetwork/main.tf
+++ 2-VirtualNetwork/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network" "todoappvirtualnetwork" {
   for_each            = var.todo
   name                = each.key
-  location            = each.value.jgh
+  location            = each.value.location
   resource_group_name = each.value.rg
   address_space       = each.value.addnet
 
