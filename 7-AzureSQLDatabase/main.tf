793ce78e372dd3ddb1faaded8b737bd95244ab2b
Abhisek Singh <77376796+absk123@users.noreply.github.com>
2023-12-26 09:15:40 +0530

    Update main.tf

--- 7-AzureSQLDatabase/main.tf
+++ 7-AzureSQLDatabase/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_sql_database" "todoappsqldatabase" {
   for_each            = var.todo
   name                = each.key
   resource_group_name = each.value.rg
-  location            = each.value.jgh
+  location            = each.value.location
   server_name         = each.value.name
 
 }
